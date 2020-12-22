778753573568ba6534c886f3c5c7189a96b09b7b
bgrbalu <bgrbalu@gmail.com>
2020-12-21 19:26:52 -0500

    Initial Commit

---
+++ src/test/java/com/cofnig/DemoConfigSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cofnig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConfigSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
